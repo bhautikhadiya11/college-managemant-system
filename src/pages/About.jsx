c976d7a71d3df8585d9a189e41132016c6d672be
bhautikhadiya11 <bhautikhadiya047@gmail.com>
2026-01-08 15:35:08 +0530

    navbar & signin changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,13 @@
 const About = () => {
   return (
+    <>
     <div className="p-10 text-center">
       <h1 className="text-3xl font-bold">About Us</h1>
       <p className="mt-4 text-gray-600">
         Our college provides quality education and modern facilities.
       </p>
     </div>
+    </>
   );
 };
 
